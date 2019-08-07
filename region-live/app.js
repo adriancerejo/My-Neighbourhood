fa4225cf4d2d9e7aa052a35f752bcbc8d6f382be
Adrian <37904908+adriancerejo@users.noreply.github.com>
2019-08-07 10:22:07 -0400

    Add files via upload

--- region-live/app.js
+++ region-live/app.js
@@ -9,6 +9,7 @@ window.addEventListener('load', () =>{
             latitude = position.coords.latitude;
             map = L.map('mapDiv').setView([latitude, longitude], 10);
             marker = L.marker([latitude, longitude]).addTo(map);
+            getInitialCountry(latitude, longitude);
             initMap(latitude, longitude);
             getWeather(latitude, longitude);
         })
@@ -21,7 +22,7 @@ function getLocation(){
 }
 
 function getCoords(inputLoc) {
-    var apikey = 'YOUR_KEY';
+    var apikey = 'XXXXXXXXXXXXXXXXXXXXXXX';
                         
     var api_url = 'https://api.opencagedata.com/geocode/v1/json'
 
@@ -41,6 +42,8 @@ function getCoords(inputLoc) {
         var data = JSON.parse(request.responseText);
         latitude = data.results[0].geometry.lat;
         longitude = data.results[0].geometry.lng;
+        country = data.results[0].components.country;
+        getNews(data.results[0].components["ISO_3166-1_alpha-2"])
         initMap(latitude, longitude);
         getWeather(latitude, longitude);
     } else if (request.status <= 500){ 
@@ -66,7 +69,7 @@ function initMap(mLat, mLong){
     map.setView([mLat, mLong], 10);
     // set map tiles source
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
         maxZoom: 18,
     }).addTo(map);
     // add marker to the map
@@ -80,7 +83,7 @@ function getWeather (mLatitude, mLongitude){
     let desc = document.querySelector(".temperature-description");
 
     const heroku = 'https://cors-anywhere.herokuapp.com/';
-    const api = `${heroku}https://api.darksky.net/forecast/YOUR_KEY/${mLatitude},${mLongitude}`
+    const api = `${heroku}https://api.darksky.net/forecast/XXXXXXXXXXXXXXXXXXXXXX/${mLatitude},${mLongitude}`
 
     fetch(api)
         .then(response =>{
@@ -101,3 +104,40 @@ function getWeather (mLatitude, mLongitude){
         return skycons.set(iconID, Skycons[currentIcon]);
     }
 };
+
+function getInitialCountry(mLatitude, mLongitude) {
+    var API_KEY = `XXXXXXXXXXXX`
+
+    var request_url = `https://api.opencagedata.com/geocode/v1/json?q=${mLatitude}%2C${mLongitude}&key=${API_KEY}&pretty=1`
+    var request = new XMLHttpRequest();
+    request.open('GET', request_url, true);
+
+    request.onload = function() {
+
+    if (request.status == 200){ 
+        var data = JSON.parse(request.responseText);
+        getNews(data.results[0].components["ISO_3166-1_alpha-2"]);
+    } else if (request.status <= 500){          
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log(data.status.message);
+    } else {
+        console.log("server error");
+    }
+    };
+
+    request.onerror = function() {
+    console.log("unable to connect to server");        
+    };
+
+    request.send();  // make the request
+}
+
+function getNews(mCountry){
+    var url = `https://newsapi.org/v2/top-headlines?country=${mCountry.toLowerCase()}&apiKey=XXXXXXXXXXXXXXXXXXXXXX`;
+    var req = new Request(url);
+    fetch(req)
+        .then(function(response) {
+            console.log(response.json());
+    })
+}
